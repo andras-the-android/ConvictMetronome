3a2893ef253205ae250979df5b99f3da40f28a45
Andras Nemeth <guni82hu@gmail.com>
2016-02-19 19:51:54 +0100

    espresso configuration

---
+++ app/src/androidTest/java/hu/kts/cmetronome/MainActivityTest.java
@@ -0,0 +1,35 @@
+package hu.kts.cmetronome;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by andrasnemeth on 19/02/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
+    @Test
+    public void testSomething() {
+        onView(withId(R.id.rep_counter))         // withId(R.id.my_view) is a ViewMatcher
+                .perform(click())               // click() is a ViewAction
+                .check(matches(isDisplayed()));
+    }
+}
