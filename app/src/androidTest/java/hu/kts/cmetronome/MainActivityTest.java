fc7c2fdfd20df3ea126c32f3a2f86f3c3bea9732
Andras Nemeth <guni82hu@gmail.com>
2016-02-22 21:05:04 +0100

    espresso sleep

--- app/src/androidTest/java/hu/kts/cmetronome/MainActivityTest.java
+++ app/src/androidTest/java/hu/kts/cmetronome/MainActivityTest.java
@@ -1,18 +1,33 @@
 package hu.kts.cmetronome;
 
+import android.os.SystemClock;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsAnything;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeoutException;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.registerIdlingResources;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by andrasnemeth on 19/02/16.
@@ -28,8 +43,32 @@ public class MainActivityTest {
 
     @Test
     public void testSomething() {
-        onView(withId(R.id.rep_counter))         // withId(R.id.my_view) is a ViewMatcher
-                .perform(click())               // click) is a ViewAction
-                .check(matches(isDisplayed()));
+        onView(withId(R.id.rep_counter))
+                .perform(click())      ;
+
+
+
+        onView(withId(R.id.rep_counter)).perform(waitId(1100l)).check(matches(withText("2")));
+
+    }
+
+    /** Perform action of waiting for a specific view id. */
+    public static ViewAction waitId(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return new IsAnything<>();
+            }
+
+            @Override
+            public String getDescription() {
+                return "waits" + millis + "millis";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                SystemClock.sleep(millis);
+            }
+        };
     }
 }
