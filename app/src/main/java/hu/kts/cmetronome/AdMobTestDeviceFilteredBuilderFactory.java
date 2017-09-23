c2026cb76b20a60ef2adcabc4f5814c7b492e9d1
Andras Nemeth <guni82hu@gmail.com>
2017-09-23 20:18:52 +0200

    update test device list

--- app/src/main/java/hu/kts/cmetronome/AdMobTestDeviceFilteredBuilderFactory.java
+++ app/src/main/java/hu/kts/cmetronome/AdMobTestDeviceFilteredBuilderFactory.java
@@ -15,7 +15,8 @@ public final class AdMobTestDeviceFilteredBuilderFactory {
         return new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .addTestDevice("21A7F45D4BDEB3BCCD96A1057F1DB48A") //Oneplus One
-                .addTestDevice("A60733E79D236E05846D3742AF9C6113") //Nexus 5
+                .addTestDevice("A60733E79D236E05846D3742AF9C6113") //Nexus 5 #1
+                .addTestDevice("458CE46583FEF19E2538289C11691B08") //Nexus 5 #2
                 .addTestDevice("3EEA5F0C49FB4B07428FD96F5998BFEB") //Alcatel POP C3
                 ;
     }
