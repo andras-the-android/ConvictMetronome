6003567382a41c30dfc1cc20ba8db5d482f0828b
Andras Nemeth <guni82hu@gmail.com>
2018-11-01 19:18:33 +0100

    consolidate analytics stub

---
+++ app/src/main/java/hu/kts/cmetronome/CmLog.kt
@@ -0,0 +1,48 @@
+package hu.kts.cmetronome
+
+import android.content.Context
+import com.google.android.gms.analytics.GoogleAnalytics
+import com.google.android.gms.analytics.HitBuilders
+import com.google.android.gms.analytics.Tracker
+import hu.kts.cmetronome.di.Injector
+import java.io.PrintWriter
+import java.io.StringWriter
+
+object CmLog {
+
+    private lateinit var tracker: Tracker
+
+    internal fun init(context: Context) {
+        createTracker(context)
+        enableTracker(context, Injector.settings.isAnalyticsEnabled)
+    }
+
+    private fun createTracker(context: Context) {
+        val analytics = GoogleAnalytics.getInstance(context)
+        tracker = analytics.newTracker(R.xml.google_analytics_tracker)
+    }
+
+    fun enableTracker(context: Context, enable: Boolean) {
+        GoogleAnalytics.getInstance(context).appOptOut = enable
+    }
+
+    fun d(tag: String, msg: String) {
+        android.util.Log.d(tag, msg)
+    }
+
+    fun e(tag: String, msg: String, th: Throwable) {
+        android.util.Log.e(tag, msg, th)
+
+        tracker.send(HitBuilders.ExceptionBuilder()
+                .setDescription(th.message + " : " + getStackTrace(th))
+                .setFatal(false)
+                .build())
+    }
+
+    private fun getStackTrace(throwable: Throwable): String {
+        val stringWriter = StringWriter()
+        val printWriter = PrintWriter(stringWriter)
+        throwable.printStackTrace(printWriter)
+        return stringWriter.toString()
+    }
+}
