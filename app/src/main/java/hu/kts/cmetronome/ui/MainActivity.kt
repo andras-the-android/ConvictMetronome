3d608f7c852efa3c0b6e8e96837addca7131443d
Andras Nemeth <guni82hu@gmail.com>
2019-12-31 01:17:30 +0100

    jetpack navigation - toolbar setup

--- app/src/main/java/hu/kts/cmetronome/ui/MainActivity.kt
+++ app/src/main/java/hu/kts/cmetronome/ui/MainActivity.kt
@@ -6,32 +6,41 @@ import android.view.MenuItem
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import hu.kts.cmetronome.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        val navController = findNavController(R.id.main_nav_controller)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener { _, _, _ -> invalidateOptionsMenu()}
     }
     
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
+        val navController = findNavController(R.id.main_nav_controller)
+        when (navController.currentDestination?.id) {
+            R.id.dest_workout -> menuInflater.inflate(R.menu.main, menu)
+            else -> menu.clear()
+        }
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_settings) {
-            openSettings()
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
+        val navController = findNavController(R.id.main_nav_controller)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
-    private fun openSettings() {
-        findNavController(R.id.main_nav_controller).navigate(R.id.go_to_settings)
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.main_nav_controller).navigateUp()
     }
 }
